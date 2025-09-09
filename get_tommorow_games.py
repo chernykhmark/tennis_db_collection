3cebbe4bc8a82f7d8417e4217e88563d4f1aa905
chernykhmark <chernykhmark77@yandex.ru>
2025-09-09 08:54:31 +0300

    build: download 140 chrome with config, add cron tasks

--- get_tommorow_games.py
+++ get_tommorow_games.py
@@ -1,15 +1,21 @@
 from driver import get_chrome_driver, driver_get_page_source, driver_get_tommorow_page_source
 from shedule import get_daily_schedule
 from database import Database
+from datetime import datetime,timedelta
+import pytz
 
 URL = 'https://www.eurosport.com/tennis/score-center.shtml'
+day = datetime.now(pytz.timezone('Europe/Moscow'))  # укажи свой часовой пояс
+tomorrow = day + timedelta(days=1)
+tomorrow_date = tomorrow.strftime('%Y-%m-%d')
 
 def main():
-    global URL
+    global URL,tomorrow_date
     db = Database()
     driver = get_chrome_driver()
-    tomorrow_page_source,dt = driver_get_tommorow_page_source(driver, URL)
-    get_daily_schedule(tomorrow_page_source,db,dt)
+    tomorrow_page_source = driver_get_tommorow_page_source(driver, URL)
+
+    get_daily_schedule(tomorrow_page_source,db,tomorrow_date)
     driver.quit()
     db.close()
 
--- run.py
+++ run.py
@@ -7,11 +7,11 @@ URL = 'https://www.eurosport.com/tennis/score-center.shtml'
 def main():
     global URL
     db = Database()
-    #driver = get_chrome_driver()
-    #page_source,dt = driver_get_page_source(driver, URL)
-    #get_daily_schedule(page_source,db,dt)
-    #driver.quit()
-    #print('Driver stop')
+    driver = get_chrome_driver()
+    page_source,dt = driver_get_page_source(driver, URL)
+    get_daily_schedule(page_source,db,dt)
+    driver.quit()
+    print('Driver stop')
     db.update_all_null_winners()
     db.close()
 
