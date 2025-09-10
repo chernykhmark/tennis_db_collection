f32bfcd8b695099e6b009dd49a1a1e8e322319a0
chernykhmark <98201915+chernykhmark@users.noreply.github.com>
2025-09-10 12:32:20 +0300

    init files to flashscore and main directory

--- flashscore/run.py
+++ flashscore/run.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from driver import driver_get_flashscore
 from datetime import datetime
 from notifications import BotSender
