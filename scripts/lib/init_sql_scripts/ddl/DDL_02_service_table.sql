a7654e6a825e540df9160413e0061887995b40ab
chernykhmark <chernykhmark77@yandex.ru>
2025-09-12 12:26:37 +0300

    delete all files about docker

--- scripts/lib/init_sql_scripts/ddl/DDL_02_service_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS stg.srv_wf_settings (
-    id serial NOT NULL PRIMARY KEY,
-    workflow_key varchar NOT NULL UNIQUE,
-    workflow_settings JSON NOT NULL
-);
--- scripts/lib/init_sql_scripts/ddl/DDL_03_object_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS stg.mongo_games (
-    id serial NOT NULL PRIMARY KEY,
-    object_id varchar NOT NULL UNIQUE,
-    object_value text NOT NULL,
-    update_ts timestamp NOT NULL
-);
